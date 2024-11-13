76191cb5c1209109aa703c026d46bd94280bea2a
CodingMaxC <max.castel123@outlook.com>
2024-11-13 10:42:32 -0500

    Navbar Added
    Added the navbar component into the home page. No pages are linked (hover animation).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
-    <div className='font-bold'>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
   )
 }
 
