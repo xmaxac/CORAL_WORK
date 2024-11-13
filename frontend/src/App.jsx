da21e964eea8a624fde2fe389ef0801fc4262adb
CodingMaxC <max.castel123@outlook.com>
2024-11-12 23:25:16 -0500

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='font-bold'>App</div>
+  )
+}
+
+export default App
