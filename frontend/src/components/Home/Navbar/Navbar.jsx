395d9abb0ecde644660ca95117ce4cd64c797033
xmaxac <max.castel123@gmail.com>
2024-11-15 18:42:07 -0500

    Updated Home Page

--- frontend/src/components/Home/Navbar/Navbar.jsx
+++ frontend/src/components/Home/Navbar/Navbar.jsx
@@ -13,26 +13,26 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className='select-none'>
       {/* Navbar */}
       <nav className='relative flex items-center h-[70px] p-6 bg-white border-b border-slate-200'>
-        <h1 className='absolute left-[2%] text-3xl select-none text-blue-500 hover:cursor-pointer' onClick={()=>navigate("/")} style={{fontFamily: 'gothamBold'}}>CoralBase</h1>
+        <h1 className='absolute left-[2%] text-3xl text-blue-500 hover:cursor-pointer' onClick={()=>navigate("/")} style={{fontFamily: 'gothamBold'}}>CoralBase</h1>
         <ul className='absolute left-[17%] flex space-x-6 items-center text-gray-600' style={{fontFamily: 'gothamLight'}}>
-          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer select-none text-blue-500'>
+          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer text-blue-500'>
             <HomeIcon className='w-[15px] mr-1'/>
             Home
           </li>
-          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer select-none'>
+          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer'>
             <UserGroupIcon className='w-[15px] mr-1'/>
             Community
           </li>
-          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer select-none'>
+          <li className='flex items-center justify-center transition-transform duration-300 ease-in-out hover:translate-y-[-0.3rem] hover:cursor-pointer'>
             <DatabaseIcon className='w-[15px] mr-1'/>
             Database
           </li>
         </ul>
         <div className='absolute w-[130px] right-[5%] p-2 rounded-md bg-blue-500 text-white items-center justify-center hover:cursor-pointer hover:bg-blue-800'>
-          <span className='flex items-center justify-center select-none	'>
+          <span className='flex items-center justify-center'>
             <PlusIcon className='w-[18px] mr-1'/>
             New Report
           </span>
